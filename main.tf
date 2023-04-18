b21986714e89ccd88c48e8265677d69003362bde
sonergzn <soner.gzn@outlook.com>
2023-04-18 03:31:36 +0200

    test persistant volume-6

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ data "google_client_config" "default" {}
 provider "kubernetes" {
   host                   = "https://${data.google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
 
 terraform {
